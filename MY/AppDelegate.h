c517da31948c792e988cbd7fd04fe5b57cdb6e9a
yao.xue <yao.xue@qunar.com>
2015-02-04 14:34:07 +0800

    Initial Commit

---
+++ MY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MY
+//
+//  Created by xueyao on 15/2/4.
+//  Copyright (c) 2015年 xueyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
